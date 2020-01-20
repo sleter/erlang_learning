1be5a348ad6005a6289ae04f6600963e7420ad19
sleter <sleter97@gmail.com>
2020-01-20 12:59:43 +0100

    Update saving sensors data

--- mag_temp_project/sensor.erl
+++ mag_temp_project/sensor.erl
@@ -1,28 +1,35 @@
 -module(sensor).
 -compile(export_all).
 
--record(state, {server, name="", delay=10000}).
+-record(state, {server, name="", delay=10000, save_iteration=5, unit="", actual_iteration=1}).
 
-loop(S = #state{server=Server, delay=Delay}) ->
+loop(S = #state{server=Server, delay=Delay, save_iteration=SI, unit=U, actual_iteration=AI}) ->
     receive
         {Server, Ref, cancel} -> Server ! {Ref, ok}
     after Delay ->
         Date = erlang:localtime(),
         Value = rand:uniform(10),
-        Server ! {update, S#state.name, Value, Date},
-        loop(S)
-end.
+        if SI == AI ->
+            Server ! {update_with_save, S#state.name, Value, Date, U},
+            loop(S#state{actual_iteration=1});
+        true ->
+            Server ! {update, S#state.name, Value, Date, U},
+            loop(S#state{actual_iteration=AI+1})
+        end
+    end.
 
-start(Name, Delay) ->
-    spawn(?MODULE, init, [self(), Name, Delay]).
+start(Name, Delay, SI, U) ->
+    spawn(?MODULE, init, [self(), Name, Delay, SI, U]).
  
-start_link(Name, Delay) ->
-    spawn_link(?MODULE, init, [self(), Name, Delay]).
+start_link(Name, Delay, SI, U) ->
+    spawn_link(?MODULE, init, [self(), Name, Delay, SI,U]).
  
-init(Server, Name, Delay) ->
+init(Server, Name, Delay, SI, U) ->
     loop(#state{server=Server,
                 name=Name,
-                delay=Delay}).
+                delay=Delay,
+                save_iteration=SI,
+                unit=U}).
 
 cancel(Pid) ->
     Ref = erlang:monitor(process, Pid),
--- mag_temp_project/weather_station.erl
+++ mag_temp_project/weather_station.erl
@@ -3,7 +3,10 @@
 
 -record(state, {sensor_values,
                 clients,
-                sensors}).
+                sensors,
+                temperature_values,
+                humidity_values,
+                ap_values}).
 
 start() ->
     register(?MODULE, Pid=spawn(?MODULE, init, [])),
@@ -18,14 +21,17 @@ terminate() ->
 
 init() ->
     % start sensors
-    _ = sensor:start_link("temperature_sensor", 10000),
-    _ = sensor:start_link("humidity_sensor", 5000),
-    _ = sensor:start_link("atmospheric_pressure_sensor", 11000),
+    _ = sensor:start_link("temperature_sensor", 10000, 11, "C (Celcius)"),
+    _ = sensor:start_link("humidity_sensor", 5000, 2, "RH (relative humidity)"),
+    _ = sensor:start_link("atmospheric_pressure_sensor", 11000, 3, "hPa"),
     % start loop
     loop(#state{
         clients=orddict:new(),
         sensors=orddict:new(),
-        sensor_values=orddict:new()}).
+        sensor_values=orddict:new(),
+        temperature_values = [],
+        humidity_values = [],
+        ap_values = []}).
 
 subscribe(Pid) ->
     Ref = erlang:monitor(process, whereis(?MODULE)),
@@ -48,6 +54,29 @@ get_info() ->
         {error, timeout}
     end.
 
+get_sensor(SN)->
+    Ref = make_ref(),
+    ?MODULE ! {self(), Ref, {get_sensor, SN}},
+    receive
+        {Ref, Msg} -> Msg
+    after 5000 ->
+        {error, timeout}
+    end.
+
+get_saved_sensor_values(SN)->
+    Ref = make_ref(),
+    ?MODULE ! {self(), Ref, {get_saved_sensor_values, SN}},
+    receive
+        {Ref, Msg} -> Msg
+    after 5000 ->
+        {error, timeout}
+    end.
+
+eq(A, B) when A =:= B ->
+    equal;
+eq(_, _) ->
+    not_equal.
+
 loop(S=#state{}) ->
     receive
         {Pid, MsgRef, {subscribe, Client}} ->
@@ -59,23 +88,80 @@ loop(S=#state{}) ->
             Pid ! {MsgRef, orddict:to_list(S#state.sensor_values)},
             io:format("Server: sensor values send to client:~p .~n", [Pid]),
             loop(S);
-        {update, Name, Value, Date} ->
+        {Pid, MsgRef, {get_sensor, SN}} ->
+            case orddict:find(SN, S#state.sensor_values) of
+                {ok, E} ->
+                    Pid ! {MsgRef, E},
+                    io:format("Server: temperature value send to client:~p .~n", [Pid]),
+                    loop(S);
+                error ->
+                    Pid ! {MsgRef, "Wrong sensor selected"},
+                    io:format("Server: wrong sensor (~p) send by client:~p .~n", [SN, Pid]),
+                    loop(S)
+            end;
+        {Pid, MsgRef, {get_saved_sensor_values, SN}} ->
+
+            if
+                SN == "temperature" ->
+                    Pid ! {MsgRef, S#state.temperature_values},
+                    io:format("Server: saved temperature values list send to client:~p .~n", [Pid]);
+                SN == "humidity" ->
+                    Pid ! {MsgRef, S#state.humidity_values},
+                    io:format("Server: saved humidity values list send to client:~p .~n", [Pid]);
+                SN == "ap" ->
+                    Pid ! {MsgRef, S#state.ap_values},
+                    io:format("Server: saved atmospheric pressure values list send to client:~p .~n", [Pid]);
+                true ->
+                    io:format("Server: wrong sensor (~p) send by client:~p .~n", [SN, Pid]),
+                    Pid ! {MsgRef, "Wrong sensor selected"}
+            end,
+            loop(S);
+        {update, Name, Value, Date, U} ->
             io:format("Server: received update from sensor: :~p .~n", [Name]),
             case Name of
                 "temperature_sensor" ->
                     Temp = orddict:store("temperature", Value, S#state.sensor_values),
-                    UpdatedValues = orddict:store("temperature_last_update", Date, Temp);
+                    Temp2 = orddict:store("temperature_unit", U, Temp),
+                    UpdatedValues = orddict:store("temperature_last_update", Date, Temp2);
                 "humidity_sensor" ->
                     Temp = orddict:store("humidity", Value, S#state.sensor_values),
-                    UpdatedValues = orddict:store("humidity_last_update", Date, Temp);
+                    Temp2 = orddict:store("humidity_unit", U, Temp),
+                    UpdatedValues = orddict:store("humidity_last_update", Date, Temp2);
                 "atmospheric_pressure_sensor" ->
                     Temp = orddict:store("atmospheric_pressure", Value, S#state.sensor_values),
-                    UpdatedValues = orddict:store("atmospheric_pressure_last_update", Date, Temp);
+                    Temp2 = orddict:store("atmospheric_pressure_unit", U, Temp),
+                    UpdatedValues = orddict:store("atmospheric_pressure_last_update", Date, Temp2);
                 _ ->
                     io:format("No match: ~p~n",[Name]),
                     UpdatedValues = S#state.sensor_values
             end,
             loop(S#state{sensor_values=UpdatedValues});
+        {update_with_save, Name, Value, Date, U} ->
+            io:format("Server: received update from sensor: :~p .~n", [Name]),
+            case Name of
+                "temperature_sensor" ->
+                    Temp = orddict:store("temperature", Value, S#state.sensor_values),
+                    Temp2 = orddict:store("temperature_unit", U, Temp),
+                    UpdatedValues = orddict:store("temperature_last_update", Date, Temp2),
+                    TVals = lists:append([S#state.temperature_values, [Value]]),
+                    loop(S#state{sensor_values=UpdatedValues, temperature_values=TVals});
+                "humidity_sensor" ->
+                    Temp = orddict:store("humidity", Value, S#state.sensor_values),
+                    Temp2 = orddict:store("humidity_unit", U, Temp),
+                    UpdatedValues = orddict:store("humidity_last_update", Date, Temp2),
+                    HVals = lists:append([S#state.humidity_values, [Value]]),
+                    loop(S#state{sensor_values=UpdatedValues, humidity_values=HVals});
+                "atmospheric_pressure_sensor" ->
+                    Temp = orddict:store("atmospheric_pressure", Value, S#state.sensor_values),
+                    Temp2 = orddict:store("atmospheric_pressure_unit", U, Temp),
+                    UpdatedValues = orddict:store("atmospheric_pressure_last_update", Date, Temp2),
+                    APVals = lists:append([S#state.ap_values, [Value]]),
+                    loop(S#state{sensor_values=UpdatedValues, ap_values=APVals});
+                _ ->
+                    io:format("No match: ~p~n",[Name]),
+                    UpdatedValues = S#state.sensor_values,
+                    loop(S#state{sensor_values=UpdatedValues})
+            end;
         shutdown ->
             exit(shutdown);
         {'DOWN', Ref, process, _Pid, _Reason} ->
